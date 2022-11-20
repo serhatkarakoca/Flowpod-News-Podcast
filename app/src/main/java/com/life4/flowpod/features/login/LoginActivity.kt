fb8b6f0c247914a6d4800ea73d3453bab48b1a45
Serhat Karakoca <serhatkarakoca@gmail.com>
2022-11-20 15:31:34 +0300

    user can reset password

--- app/src/main/java/com/life4/flowpod/features/login/LoginActivity.kt
+++ app/src/main/java/com/life4/flowpod/features/login/LoginActivity.kt
@@ -7,14 +7,17 @@ import android.graphics.Shader
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Patterns
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
@@ -25,6 +28,7 @@ import com.life4.core.extensions.observe
 import com.life4.flowpod.R
 import com.life4.flowpod.data.MyPreference
 import com.life4.flowpod.databinding.ActivityLoginBinding
+import com.life4.flowpod.databinding.BottomSheetResetPasswordBinding
 import com.life4.flowpod.features.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -47,6 +51,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layou
         }
 
         auth = FirebaseAuth.getInstance()
+        auth.useAppLanguage()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -67,6 +72,11 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layou
 
             getBinding().tvWelcome.paint.shader = textShader
         }
+
+
+    }
+
+    override fun setupListener() {
 
         getBinding().buttonSignIn.setOnClickListener {
             googleSignInClient.signOut()
@@ -118,6 +128,35 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layou
             viewModel.signInMode.value?.let {
                 viewModel.signInMode.value = !it
             }
+        }
+
+        getBinding().tvResetPassword.setOnClickListener {
+            BottomSheetDialog(this).apply {
+                val inflater = LayoutInflater.from(this@LoginActivity)
+                val binding = DataBindingUtil.inflate<BottomSheetResetPasswordBinding>(
+                    inflater,
+                    R.layout.bottom_sheet_reset_password,
+                    null,
+                    false
+                )
+                binding.btnLogin.setOnClickListener {
+                    if (isValidEmail(binding.etEmail.text.toString())) {
+                        auth.sendPasswordResetEmail(binding.etEmail.text.toString())
+                            .addOnCompleteListener {
+                                showInfoDialog(
+                                    getString(R.string.success_reset_password), getString(
+                                        R.string.success
+                                    )
+                                )
+                            }
+                        dismiss()
+                    } else {
+                        binding.emailLayout.isErrorEnabled = true
+                        binding.emailLayout.error = getString(R.string.invalid_email)
+                    }
+                }
+                setContentView(binding.root)
+            }.show()
         }
     }
 
